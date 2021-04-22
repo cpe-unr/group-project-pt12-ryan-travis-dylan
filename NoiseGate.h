8fcdde9cc9eeb4ee53546f77339797b5b585c4f6
Dylan Judy <djudy1018@gmail.com>
2021-04-22 13:47:59 -0700

    Getting there.

--- NoiseGate.h
+++ NoiseGate.h
@@ -28,5 +28,12 @@ template <typename B> void NoiseGate<B>::processBuffer(B* buffer, int bufferSize
 		
 	}
 
+	for( int i=0; i<bufferSize; i++){
+		if(buffer[i] < (128+5) && (buffer[i] > (128-5))){
+			buffer[i] = 128;
+		}
+		
+	}
+
 }
 #endif
--- Normalizer.h
+++ Normalizer.h
@@ -19,4 +19,20 @@ public:
 	Normalizer(int threshold);
    	void processBuffer(B* buffer,int bufferSize);
 };
+template <typename B>Normalizer<B>::Normalizer(int threshold): threshold(threshold){}
+template <typename B>void Normalizer<B>::processBuffer(B* buffer,int bufferSize){
+	int max=0;
+	for(int i=0; i<bufferSize; i++){
+		if(buffer[i]<max){
+			buffer[i] = max;
+		}
+	}
+	if(B == unsigned char){
+		buffer*(255/max);
+	}
+	if(B == signed int){
+		buffer*(max/32767);
+	}
+
+
 #endif
