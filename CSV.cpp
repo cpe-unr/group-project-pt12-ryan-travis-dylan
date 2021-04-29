8ba0fea94c225c7590e139c0a4f7d9f1578ab99e
traviscarter24 <traviscarter@nevada.unr.edu>
2021-04-29 14:42:48 -0700

    CSV file

--- CSV.cpp
+++ CSV.cpp
@@ -4,3 +4,23 @@
 #include <cstdint>
 #include <cmath>
 #include <limits>
+
+using namespace std;
+
+void CSV::outputCSV(string OUTPUT, string CSVFile1, string CSVFile2, string CSVFile3, string CSVFile4){
+
+OUTPUT=OUTPUT+".csv";
+std::ofstream myFile(OUTPUT);
+
+myFile<<",Artist,Track,Album,Data,Genre,Track Number,Comments,";
+myFile<<"\n";
+myFile<<CSVFile1;
+myFile<<"\n";
+myFile<<CSVFile2;
+myFile<<"\n";
+myFile<<CSVFile3;
+myFile<<"\n";
+myFile<<CSVFile4;
+
+myFile.close();
+}
